46035261380bc08ea5952dfbf440fbdbaedcc3a7
Roger <rogersanttosxx@gmail.com>
2022-05-19 14:17:14 -0300

    paginas_static

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Otávio',
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+    'name': 'Roger santos',
+})
