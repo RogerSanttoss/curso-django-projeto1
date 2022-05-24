ce0dfe66bd7d604134d85001c31d83c8f06d4720
Roger <rogersanttosxx@gmail.com>
2022-05-24 12:45:28 -0300

    erro_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+            is_published=True,
+        ).order_by('-id')
+        
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
+        'recipes': recipes,
     })
 
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
+    })
+
+
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id, is_published=True,
+    )
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-    'name': 'Roger santos',
-})
+        'recipe': recipe,
+        'is_detail_page': True,
+    })
